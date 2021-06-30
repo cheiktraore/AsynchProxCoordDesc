431bbd03c61a7c53644c209e332ceae8339eecf7
mathurinm <mathurin.massias@gmail.com>
2021-06-30 17:05:37 +0200

    rm comments, flake8, change solver name

--- asyn_prox_coord_desc.py
+++ asyn_prox_coord_desc.py
@@ -2,25 +2,24 @@ import numpy as np
 from numba import njit
 from numpy.linalg import norm
 from multiprocessing import shared_memory
-# import time
+
 
 @njit
 def ST(x, u):
     """Soft-thresholding of vector x at level u, i.e., entrywise:
     x_i + u_i if x_i < -u_i, x_i - u_i if x_i > u_i and 0 else.
     """
-    # TODO implement without if, using only np.abs, np.sign ad np.maximum
     return np.sign(x) * np.maximum(0., np.abs(x) - u)
 
 
 @njit
 def lasso_loss(A, b, lbda, x):
     """Value of Lasso objective at x."""
-    # TODO implement with the norm function imported above/
     return norm(A @ x - b) ** 2 / 2. + lbda * norm(x, ord=1)
 
 
-def choose_coord(n_features, low_var=3, high_var=19, low_mean=6, high_mean=19, proc_nums=9):
+def choose_coord(n_features, low_var=3, high_var=19, low_mean=6, high_mean=19,
+                 proc_nums=9):
     # sigma = np.random.randint(low_var,high_var)
     # mu = np.random.randint(low_mean,high_mean)
     # i_k = sigma * np.random.randn() + mu
@@ -29,7 +28,7 @@ def choose_coord(n_features, low_var=3, high_var=19, low_mean=6, high_mean=19, p
     return i_k
 
 
-def PRBCFB(A, b, x, lbda, max_iteration, max_it, i_k):
+def cd_lasso(A, b, x, lbda, max_iteration, max_it, i_k):
     A = np.asfortranarray(A)
     n, m = A.shape
     gammas = 1. / norm(A, axis=0) ** 2
@@ -45,7 +44,7 @@ def PRBCFB(A, b, x, lbda, max_iteration, max_it, i_k):
     while ((max_iteration[0]-1) > 0):
         old_x = x.copy()
         x[indx] = ST(x[indx] - A[:, indx].T @ u * gammas[indx],
-                        lbda * gammas[indx])
+                     lbda * gammas[indx])
         u += A[:, indx] * (x[indx] - old_x[indx])
         try:
             max_iteration[0] = max_iteration[0] - 1
