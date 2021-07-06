d7115089c7c0d9e70a3d7550f8c06d6636bfd702
Cheik Traoré <cheik.traore@edu.unige.it>
2021-07-06 11:23:46 +0200

    change arrays to ints for max_iter, t and text_var

--- main.py
+++ main.py
@@ -6,28 +6,24 @@ from multiprocessing import cpu_count
 from utils import ST, choose_coord, lasso_loss, save_object
 
 from celer.datasets import make_correlated_data
-# from celer.utils import configure_plt
-# configure_plt()
 
 n, m, s = 50, 100, 5
 
-max_iter = 10_000
-# max_it = max_iter[0]
+max_iter = 60_000
+max_it = max_iter
 
 A, y, x_true = make_correlated_data(n, m, density=0.1, random_state=0)
 lbda_max = norm(A.T @ y, ord=np.inf)
 lbda = lbda_max / 100
-#
+
 N_PROC = cpu_count()
 x = np.zeros(m)
 R = y.copy()
 lc = norm(A, axis=0) ** 2
 all_x = np.zeros((max_iter, m))
-# fx = np.zeros(max_iter)
-# t = np.array([0])
-
-# TODO maybe a closure and the threads are started inside
-
+fx = np.zeros(max_iter)
+t = 0
+test_var = 0
 
 def cd_concu(A, y, lbda):
     global max_iter
@@ -36,25 +32,25 @@ def cd_concu(A, y, lbda):
     global lc
     global all_x
     global t
+    global max_it
+    global test_var
 
-    print(max_iter)
     while max_iter > 0:
-        max_iter -= 1
         i = choose_coord(n_features=m)
         old_xi = x[i]
         neg_grad_i = A[:, i] @ R
         new = ST(x[i] + neg_grad_i / lc[i], lbda / lc[i])
         x[i] = new
         R += A[:, i] * (old_xi - new)
-    #     if t[0] >= max_it:
-    #         break
-    #     fx[t[0]] = lasso_loss(A, y, lbda, x)
-    #     all_x[t[0]] = x
-    #     t[0] += 1
-    #     max_iter[0] -= 1
-    # if test_var[0] == 0:
-    #     test_var[0] += 1
-    #     save_object([A, y, lbda, x, all_x, fx])
+        if t >= max_it:
+            break
+        fx[t] = lasso_loss(A, y, lbda, x)
+        all_x[t] = x
+        t += 1
+        max_iter -= 1
+    if test_var == 0:
+        test_var += 1
+        save_object([A, y, lbda, x, all_x, fx])
 
 
 threads = [threading.Thread(target=cd_concu, args=(A, y, lbda))
@@ -63,9 +59,5 @@ threads = [threading.Thread(target=cd_concu, args=(A, y, lbda))
 for thread in threads:
     thread.start()
 
-
 for thread in threads:
     thread.join()
-
-
-print(f"MAX ITER: {max_iter}")
